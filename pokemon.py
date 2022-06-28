c0156dd6a398999a8a7b2eb7959f989f58cdb8bb
bri-personal <107887789+bri-personal@users.noreply.github.com>
2022-06-27 22:59:01 -0700

    added fields to pokemon

--- pokemon.py
+++ pokemon.py
@@ -1,9 +1,19 @@
 import random
 
 class Pokemon:
-    def __init__(self,name):
-        self.name=name
-        self.num=random.randrange(100) #temporary attribute for testing
-    
+    def __init__(self,species,level):
+        self.nickname=species
+        self.species=species
+        self.level=level
+
+        self.ivs=[]
+        for _ in range(6):
+            self.ivs.append(random.randrange(32))
+        
+        self.evs=[0]*6
+
+        self.stats=[]
+        #calculate stats based on base stats
+
     def __repr__(self):
-        return self.name+str(self.num)
+        return self.species+" level "+str(self.level)
