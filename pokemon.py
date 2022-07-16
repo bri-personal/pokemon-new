8b97cca1fa6665edacf7fd5f33e38f7ca7baac2c
bri-personal <107887789+bri-personal@users.noreply.github.com>
2022-07-16 13:49:23 -0700

    added rest of text to stats tab

--- pokemon.py
+++ pokemon.py
@@ -6,6 +6,15 @@ class Pokemon:
         self.species=species
         self.level=level
         self.gender= 'male' if random.random()>0.5 else 'female' #50/50 for all for now, change for specific species
+        
+        self.nature='Nature '+str(random.randrange(32))
+        self.item='None'
+        self.ability='Ability '+str(random.randrange(100))
+
+        #up to four moves
+        self.moves=[]
+        for i in range(4):
+            self.moves.append(str(i)+': Move '+str(random.randrange(100)))
 
         #individual values - stats determined on init
         self.ivs=[]
@@ -18,6 +27,8 @@ class Pokemon:
         #final stats calculated from level, ivs, evs, etc
         self.stats=[]
         #calculate stats based on base stats
+        for i in range(len(self.ivs)):
+            self.stats.append(self.level*self.ivs[i]+self.evs[i])
 
     def __repr__(self):
         return self.species+" level "+str(self.level)
--- stats_tab.py
+++ stats_tab.py
@@ -1,3 +1,4 @@
+from pstats import Stats
 import pygame
 from settings import *
 from sprites import draw_text
@@ -23,26 +24,83 @@ class StatsTab:
         self.pokemon=pokemon
         self.image.fill(WHITE)
 
+        #keep track of y of next element on tab and increment with height of each element so if height of one thing changes it doesn't affect rest
+        #if changing size of any element, change number added to y as well
+        y=0 
+
         #top bar of tab
-        pygame.draw.rect(self.image,LIGHT_GRAY,(0,0,StatsTab.TAB_WIDTH,StatsTab.TAB_WIDTH*3//20))
+        pygame.draw.rect(self.image,LIGHT_GRAY,(0,y,StatsTab.TAB_WIDTH,StatsTab.TAB_WIDTH*3//20))
         #draw ball image
-        pygame.draw.circle(self.image,BLACK,(StatsTab.TAB_WIDTH//40+StatsTab.TAB_WIDTH//20,StatsTab.TAB_WIDTH*3//40),StatsTab.TAB_WIDTH//20,3)
+        pygame.draw.circle(self.image,BLACK,(StatsTab.TAB_WIDTH//40+StatsTab.TAB_WIDTH//20,y+StatsTab.TAB_WIDTH*3//40),StatsTab.TAB_WIDTH//20,3)
         #name
-        draw_text(self.image,pokemon.nickname,StatsTab.TAB_WIDTH*3//40,BLACK,StatsTab.TAB_WIDTH//20+StatsTab.TAB_WIDTH//10,StatsTab.TAB_WIDTH//30,'topleft')
+        draw_text(self.image,pokemon.nickname,StatsTab.TAB_WIDTH*3//40,BLACK,StatsTab.TAB_WIDTH//20+StatsTab.TAB_WIDTH//10,y+StatsTab.TAB_WIDTH//30,'topleft')
         #draw gender image
         color=BLUE if self.pokemon.gender=='male' else RED
-        pygame.draw.circle(self.image,color,(StatsTab.TAB_WIDTH-(StatsTab.TAB_WIDTH//40+StatsTab.TAB_WIDTH//20),StatsTab.TAB_WIDTH*3//40),StatsTab.TAB_WIDTH//20,3)
+        pygame.draw.circle(self.image,color,(StatsTab.TAB_WIDTH-(StatsTab.TAB_WIDTH//40+StatsTab.TAB_WIDTH//20),y+StatsTab.TAB_WIDTH*3//40),StatsTab.TAB_WIDTH//20,3)
         #level
-        draw_text(self.image,"Lv. "+str(pokemon.level),StatsTab.TAB_WIDTH*3//40,BLACK,StatsTab.TAB_WIDTH-(StatsTab.TAB_WIDTH//20+StatsTab.TAB_WIDTH//10),StatsTab.TAB_WIDTH//30,'topright')
+        draw_text(self.image,"Lv. "+str(pokemon.level),StatsTab.TAB_WIDTH*3//40,BLACK,StatsTab.TAB_WIDTH-(StatsTab.TAB_WIDTH//20+StatsTab.TAB_WIDTH//10),y+StatsTab.TAB_WIDTH//30,'topright')
+
+        y+=StatsTab.TAB_WIDTH*3//20+StatsTab.TAB_WIDTH//40 #top box height + spacing
 
         #middle part of tab
         #species/number
-        draw_text(self.image,pokemon.species+" - No. "+"0",StatsTab.TAB_WIDTH//20,BLACK,StatsTab.TAB_WIDTH//40,StatsTab.TAB_WIDTH*3//20+StatsTab.TAB_WIDTH//40,'topleft')
+        draw_text(self.image,pokemon.species+" - No. "+"0",StatsTab.TAB_WIDTH*5//80,BLACK,StatsTab.TAB_WIDTH//40,y,'topleft')
+
+        y+=StatsTab.TAB_WIDTH//10
+
         #types
-        pygame.draw.rect(self.image,GRAY,(StatsTab.TAB_WIDTH//40,StatsTab.TAB_WIDTH*3//20+StatsTab.TAB_WIDTH*5//40,StatsTab.TAB_WIDTH//2-StatsTab.TAB_WIDTH//20,StatsTab.TAB_WIDTH//20+StatsTab.TAB_WIDTH//10))
-        pygame.draw.rect(self.image,WHITE,(StatsTab.TAB_WIDTH//20,StatsTab.TAB_WIDTH*3//20+StatsTab.TAB_WIDTH*3//20,StatsTab.TAB_WIDTH//10,StatsTab.TAB_WIDTH//10),3)
-        draw_text(self.image,"Type 1",StatsTab.TAB_WIDTH*3//40,WHITE,StatsTab.TAB_WIDTH//20+StatsTab.TAB_WIDTH//10+StatsTab.TAB_WIDTH//40,StatsTab.TAB_WIDTH*3//20+StatsTab.TAB_WIDTH*8//40,'midleft')
+        pygame.draw.rect(self.image,GRAY,(StatsTab.TAB_WIDTH//40,y,StatsTab.TAB_WIDTH//2-StatsTab.TAB_WIDTH//20,StatsTab.TAB_WIDTH*3//20))
+        pygame.draw.rect(self.image,WHITE,(StatsTab.TAB_WIDTH//20,y+StatsTab.TAB_WIDTH//40,StatsTab.TAB_WIDTH//10,StatsTab.TAB_WIDTH//10),3)
+        draw_text(self.image,"Type 1",StatsTab.TAB_WIDTH*3//40,WHITE,StatsTab.TAB_WIDTH//20+StatsTab.TAB_WIDTH//10+StatsTab.TAB_WIDTH//40,y+StatsTab.TAB_WIDTH*3//40,'midleft')
         if True: #show second type if applicable
-            pygame.draw.rect(self.image,GRAY,(StatsTab.TAB_WIDTH//40+StatsTab.TAB_WIDTH//2,StatsTab.TAB_WIDTH*3//20+StatsTab.TAB_WIDTH*5//40,StatsTab.TAB_WIDTH//2-StatsTab.TAB_WIDTH//20,StatsTab.TAB_WIDTH//20+StatsTab.TAB_WIDTH//10))
-            pygame.draw.rect(self.image,WHITE,(StatsTab.TAB_WIDTH//20+StatsTab.TAB_WIDTH//2,StatsTab.TAB_WIDTH*3//20+StatsTab.TAB_WIDTH*3//20,StatsTab.TAB_WIDTH//10,StatsTab.TAB_WIDTH//10),3)
-            draw_text(self.image,"Type 2",StatsTab.TAB_WIDTH*3//40,WHITE,StatsTab.TAB_WIDTH//20+StatsTab.TAB_WIDTH//10+StatsTab.TAB_WIDTH//40+StatsTab.TAB_WIDTH//2,StatsTab.TAB_WIDTH*3//20+StatsTab.TAB_WIDTH*8//40,'midleft')
+            pygame.draw.rect(self.image,GRAY,(StatsTab.TAB_WIDTH//40+StatsTab.TAB_WIDTH//2,y,StatsTab.TAB_WIDTH//2-StatsTab.TAB_WIDTH//20,StatsTab.TAB_WIDTH*3//20))
+            pygame.draw.rect(self.image,WHITE,(StatsTab.TAB_WIDTH//20+StatsTab.TAB_WIDTH//2,y+StatsTab.TAB_WIDTH//40,StatsTab.TAB_WIDTH//10,StatsTab.TAB_WIDTH//10),3)
+            draw_text(self.image,"Type 2",StatsTab.TAB_WIDTH*3//40,WHITE,StatsTab.TAB_WIDTH//20+StatsTab.TAB_WIDTH//10+StatsTab.TAB_WIDTH//40+StatsTab.TAB_WIDTH//2,y+StatsTab.TAB_WIDTH*3//40,'midleft')
+
+        y+=StatsTab.TAB_WIDTH*3//20+StatsTab.TAB_WIDTH//20
+
+        pygame.draw.line(self.image,LIGHT_GRAY,(StatsTab.TAB_WIDTH//40,y),(StatsTab.TAB_WIDTH-StatsTab.TAB_WIDTH//40,y),1)
+
+        y+=StatsTab.TAB_WIDTH//40
+
+        #stats and other info
+        stat_text=['HP:         ', 'ATK:       ', 'DEF:       ', 'SPATK:   ', 'SPDEF:  ', 'SPD:       ']
+        for i in range(len(stat_text)):
+            draw_text(self.image,stat_text[i]+str(self.pokemon.stats[i]),StatsTab.TAB_WIDTH*5//80,BLACK,StatsTab.TAB_WIDTH//40,y,'topleft')
+            y+=StatsTab.TAB_WIDTH*5//80+StatsTab.TAB_WIDTH//40
+
+        y+=StatsTab.TAB_WIDTH//40
+
+        draw_text(self.image,self.pokemon.nature+' by nature',StatsTab.TAB_WIDTH*5//80,BLACK,StatsTab.TAB_WIDTH//40,y,'topleft')
+
+        y+=StatsTab.TAB_WIDTH*5//80+StatsTab.TAB_WIDTH//40
+
+        draw_text(self.image,'Held item: '+self.pokemon.item,StatsTab.TAB_WIDTH*5//80,BLACK,StatsTab.TAB_WIDTH//40,y,'topleft')
+
+        y+=StatsTab.TAB_WIDTH*5//80+StatsTab.TAB_WIDTH//40
+
+        draw_text(self.image,'Ability: '+self.pokemon.ability,StatsTab.TAB_WIDTH*5//80,BLACK,StatsTab.TAB_WIDTH//40,y,'topleft')
+
+        y+=StatsTab.TAB_WIDTH*5//80+StatsTab.TAB_WIDTH//20
+
+        pygame.draw.line(self.image,LIGHT_GRAY,(StatsTab.TAB_WIDTH//40,y),(StatsTab.TAB_WIDTH-StatsTab.TAB_WIDTH//40,y),1)
+
+        y+=StatsTab.TAB_WIDTH//40
+
+        #moves
+        draw_text(self.image,'Moves:',StatsTab.TAB_WIDTH*5//80,BLACK,StatsTab.TAB_WIDTH//40,y,'topleft')
+
+        y+=StatsTab.TAB_WIDTH*5//80+StatsTab.TAB_WIDTH//40
+        
+        for i in range(4):
+            if i<len(self.pokemon.moves):
+                pygame.draw.rect(self.image,GRAY,(StatsTab.TAB_WIDTH//40,y,StatsTab.TAB_WIDTH-StatsTab.TAB_WIDTH//20,StatsTab.TAB_WIDTH*3//20))
+                pygame.draw.rect(self.image,WHITE,(StatsTab.TAB_WIDTH//20,y+StatsTab.TAB_WIDTH//40,StatsTab.TAB_WIDTH//10,StatsTab.TAB_WIDTH//10),3)
+                draw_text(self.image,self.pokemon.moves[i],StatsTab.TAB_WIDTH*3//40,WHITE,StatsTab.TAB_WIDTH//20+StatsTab.TAB_WIDTH//10+StatsTab.TAB_WIDTH//40,y+StatsTab.TAB_WIDTH*3//40,'midleft')
+            y+=StatsTab.TAB_WIDTH//20+StatsTab.TAB_WIDTH//10+StatsTab.TAB_WIDTH//40
+
+        #markings
+        for i in range(6):
+            pygame.draw.rect(self.image,LIGHT_GRAY,(StatsTab.TAB_WIDTH//2-3*StatsTab.TAB_WIDTH//20-5*StatsTab.TAB_WIDTH//40//2+i*(StatsTab.TAB_WIDTH//20+StatsTab.TAB_WIDTH//40),y,StatsTab.TAB_WIDTH//20,StatsTab.TAB_WIDTH//20))
+
+        
