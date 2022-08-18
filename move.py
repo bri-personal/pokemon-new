f81c05d58c30bbc8a1ae4bf63a5a16287f2519e8
bri-personal <107887789+bri-personal@users.noreply.github.com>
2022-08-18 10:16:46 -0700

    added field to Move class to determine if move is physical or special

--- move.py
+++ move.py
@@ -1,7 +1,8 @@
 class Move:
-    def __init__(self,name:str,move_type:str,power:int,accuracy:float,pp:int,max_pp:int,priority:int):
+    def __init__(self,name:str,move_type:str,atk_type:str,power:int,accuracy:float,pp:int,max_pp:int,priority:int):
         self.name=name
-        self.type=move_type
+        self.type=move_type #elemental type of move, used to calculate effectiveness
+        self.atk_type=atk_type #physical or special move, determines which attack stat is used for dmg
         self.power=power #int power factor to user in damage calculation
         self.accuracy=accuracy #float btw 0 and 1, move hits if random.random() is less than this
         self.pp=pp #int number of times move can be used
@@ -10,12 +11,12 @@ class Move:
 
 #dictionary of all move names matched to respective move objects
 #for now, priority=0 for all of them but change later
-ALL_MOVES={'Tackle':        Move('Tackle',  'normal',40,1,35,56,0),
-           'Quick Attack':  Move('Quick Attack',  'normal',40,1,35,56,0),
-           'Vine Whip':     Move('Vine Whip','grass',45,1,25,40,0),
-           'Ember':         Move('Ember',     'fire',40,1,25,40,0),
-           'Water Gun':     Move('Water Gun','water',40,1,25,40,0),
-           'Thunder Shock': Move('Thunder Shock','electric',40,1,40,48,0)
+ALL_MOVES={'Tackle':        Move('Tackle','normal','physical',40,1,35,56,0),
+           'Quick Attack':  Move('Quick Attack','normal','physical',40,1,35,56,0),
+           'Vine Whip':     Move('Vine Whip','grass','physical',45,1,25,40,0),
+           'Ember':         Move('Ember','fire','special',40,1,25,40,0),
+           'Water Gun':     Move('Water Gun','water','special',40,1,25,40,0),
+           'Thunder Shock': Move('Thunder Shock','electric','special',40,1,40,48,0)
           }
 
 
